efcea09b078f05e61a356d8b4be4e7bf9a222221
Telehakke <122124514+Telehakke@users.noreply.github.com>
2026-08-03 12:58:02 +0900

    260803c

--- src/App.tsx
+++ src/App.tsx
@@ -53,7 +53,7 @@ const Home = (): JSX.Element => {
                 <ImageFilesOpenButton />
                 <ZipFileOpenButton />
             </div>
-            <p className="fixed bottom-8 left-8">v0.260803b</p>
+            <p className="fixed bottom-8 left-8">v0.260803c</p>
         </>
     );
 };
